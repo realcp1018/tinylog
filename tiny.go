82d75a1fb0700df604a347e819b1a9aafde72959
realcp1018 <forevermessi@foxmail.com>
2023-06-13 18:20:10 +0800

    make file Logger configurable

--- tiny.go
+++ tiny.go
@@ -9,6 +9,7 @@ import (
 	"gopkg.in/natefinch/lumberjack.v2"
 	"os"
 	"runtime/debug"
+	"strings"
 	"sync"
 )
 
@@ -22,8 +23,11 @@ const (
 
 type TinyLogger struct {
 	*Logger
+	// mutex add for SetPrefix
 	mu       sync.Mutex
 	logLevel uint8
+	// in case to reconfig lumberjack.Logger, we store filename here
+	filename string
 }
 
 func NewFileLogger(fileName string, level uint8) *TinyLogger {
@@ -36,14 +40,36 @@ func NewFileLogger(fileName string, level uint8) *TinyLogger {
 		Compress:   true,
 	})
 	logger.SetFlags(LstdFlags | Lmicroseconds | Lshortfile | Lmsgprefix)
-	return &TinyLogger{Logger: logger, logLevel: level}
+	return &TinyLogger{
+		Logger:   logger,
+		logLevel: level,
+		filename: fileName,
+	}
 }
 
 func NewStreamLogger(level uint8) *TinyLogger {
 	logger := new(Logger)
 	logger.SetOutput(os.Stdout)
 	logger.SetFlags(LstdFlags | Lmicroseconds | Lmsgprefix | Lshortfile)
-	return &TinyLogger{Logger: logger, logLevel: level}
+	return &TinyLogger{
+		Logger:   logger,
+		logLevel: level,
+	}
+}
+
+func (l *TinyLogger) SetFileConfig(maxSizeMb, maxBackupCount, maxKeepDays int) {
+	switch l.Writer().(type) {
+	case *lumberjack.Logger:
+		l.SetOutput(&lumberjack.Logger{
+			Filename:   l.filename,
+			MaxSize:    maxSizeMb,
+			MaxBackups: maxBackupCount,
+			MaxAge:     maxKeepDays,
+			Compress:   true,
+		})
+	default:
+		return
+	}
 }
 
 func (l *TinyLogger) SetLevel(level uint8) {
@@ -54,49 +80,71 @@ func (l *TinyLogger) GetLevel() uint8 {
 	return l.logLevel
 }
 
-func (l *TinyLogger) Debug(logStr string, v ...interface{}) {
+func (l *TinyLogger) Debug(format string, v ...interface{}) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	if l.logLevel == DEBUG {
 		l.SetPrefix(fmt.Sprintf("[DEBUG] "))
-		_ = l.Output(2, fmt.Sprintf(logStr, v...))
+		_ = l.Output(2, fmt.Sprintf(format, v...))
 	}
 }
 
-func (l *TinyLogger) Info(logStr string, v ...interface{}) {
+func (l *TinyLogger) Info(format string, v ...interface{}) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	if l.logLevel <= INFO {
 		l.SetPrefix("[INFO] ")
-		_ = l.Output(2, fmt.Sprintf(logStr, v...))
+		_ = l.Output(2, fmt.Sprintf(format, v...))
 	}
 }
 
-func (l *TinyLogger) Warn(logStr string, v ...interface{}) {
+func (l *TinyLogger) Warn(format string, v ...interface{}) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	if l.logLevel <= WARN {
 		l.SetPrefix("[WARN] ")
-		_ = l.Output(2, fmt.Sprintf(logStr, v...))
+		_ = l.Output(2, fmt.Sprintf(format, v...))
 	}
 }
 
-func (l *TinyLogger) Error(logStr string, v ...interface{}) {
+func (l *TinyLogger) Error(format string, v ...interface{}) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	if l.logLevel <= ERROR {
 		l.SetPrefix("[ERROR] ")
-		_ = l.Output(2, fmt.Sprintf("%s [stacktrace]:\n%s", fmt.Sprintf(logStr, v...), string(debug.Stack())))
+		_ = l.Output(2, fmt.Sprintf("%s\n[stacktrace]:\n%s", fmt.Sprintf(format, v...), string(debug.Stack())))
 	}
 }
 
 // Fatal do exit
-func (l *TinyLogger) Fatal(logStr string, v ...interface{}) {
+func (l *TinyLogger) Fatal(format string, v ...interface{}) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	if l.logLevel <= FATAL {
 		l.SetPrefix("[FATAL] ")
-		_ = l.Output(2, fmt.Sprintf("%s [stacktrace]:\n%s", fmt.Sprintf(logStr, v...), string(debug.Stack())))
+		_ = l.Output(2, fmt.Sprintf("%s\n[stacktrace]:\n%s", fmt.Sprintf(format, v...), string(debug.Stack())))
 		os.Exit(1)
 	}
+}
+
+// Add some common print interface functions
+func (l *TinyLogger) Print(v ...interface{}) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	if l.logLevel <= INFO {
+		l.SetPrefix("[INFO] ")
+		var format string
+		for i := 0; i < len(v); i++ {
+			format += "%v "
+		}
+		_ = l.Output(2, fmt.Sprintf(strings.TrimSpace(format), v...))
+	}
+}
+
+func (l *TinyLogger) Printf(format string, v ...interface{}) {
+	l.Info(format, v...)
+}
+
+func (l *TinyLogger) Println(v ...interface{}) {
+	l.Print(v, "\n")
 }
--- tinylog_test.go
+++ tinylog_test.go
@@ -1,16 +1,16 @@
 package tinylog
 
 import (
-	"fmt"
 	"testing"
 )
 
 func Test_tinylog(t *testing.T) {
-	logger := NewStreamLogger(INFO)
-	// debug msg will not show because level is INFO
-	logger.Debug("this is [%s] msg.", "debug")
-	logger.Info("this is [%s] msg.", "info")
-	logger.Warn("this is [%s] msg!", "warn")
-	logger.Error("this is [%s] msg!", "error")
-	fmt.Printf("%v %T %v\n", logger.Flags(), logger.Writer(), logger.Prefix())
+	var logger *TinyLogger
+
+	logger = NewStreamLogger(INFO)
+	logger.Info("this a %s log in stdout", "INFO")
+
+	logger = NewFileLogger("111.log", INFO)
+	logger.SetFileConfig(1, 1, 1)
+	logger.Info("this a %s log in file", "INFO")
 }
